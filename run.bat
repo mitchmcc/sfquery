ff9cf0183004c1de2ba3adb7c5dd64d3214be99d
Mitchell McConnell <mitchmcconnell2@outlook.com>
2016-10-04 08:48:22 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,2 @@
 @echo off
-rem python sfquery.py -u mitch.mcconnell@bhn.com.uat -p Scrappy5312 -t K6bdpgKdmveedg186zLWVhI3x -v %*
-python sfquery.py -u mitch.mcconnell@bhn.com.uat -p Scrappy5312 -t K6bdpgKdmveedg186zLWVhI3x %*
+python sfquery.py -u sfusername -p sfpassword -t sftoken %*
